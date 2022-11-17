7e4b454f9f15ca3eaa451628924e6d9b556b0ab2
Adam Shen <inbox.adam@gmail.com>
2022-11-17 15:25:16 -0800

    update

--- jsonality.js
+++ jsonality.js
@@ -2,8 +2,10 @@
 	$.fn.jsonality = function (option) {
 		console.log('Quiz Ready');
 		const $This = this,
-			page = option;
-		let answer = [
+			page = $This.data('quiz-set'),
+			defaultSet = 'emotions',
+			testSet = page ? page : defaultSet;
+		let genericAnswer = [
 			{ value: 6, label: 'Strongly agree' },
 			{ value: 5, label: 'Agree' },
 			{ value: 4, label: 'Agree a little' },
@@ -13,24 +15,61 @@
 		];
 
 		let quiz = {
-			fruit: [
+			xmas: [
 				{
-					question: 'An Apple a day ...',
+					question: 'Which popular Christmas beverage is also called "milk punch?"',
+					answer: [
+						{value: 0, label: 'Coffee'},
+						{value: 1, label: 'Eggnog'},
+						{value: 0, label: 'Milk and Cookie'},
+						{value: 0, label: 'Dr Christmas'}
+					]
 				},
 				{
-					question: 'Grapes makes great wine',
+					question: 'Which reindeer isn&rsquo;t Santa&rsquo;s',
+					answer: [
+						{value: 0, label: 'Dasher'},
+						{value: 0, label: 'Comet '},
+						{value: 0, label: 'Dancer'},
+						{value: 1, label: 'Snoopy'}
+						{value: 0, label: 'Prancer'},
+					]
 				},
 				{
-					question: 'Tomato is a fruit',
+					question: 'How many ghosts show up in <em>A Christmas Carol?</em>',
+					answer: [
+						{value: 0, label: '2'},
+						{value: 0, label: '3'},
+						{value: 1, label: '4'},
+						{value: 0, label: '5'}
+					]
 				},
 				{
-					question: 'Pumpkin makes pie',
+					question: 'What are the other most popular names for Santa Claus?',
+					answer: [
+						{value: 0, label: 'Charlie Brown'},
+						{value: 1, label: 'Kris Kringle'},
+						{value: 1, label: 'Saint Nick'},
+						{value: 0, label: 'Morgan Freeman'}
+					]
 				},
 				{
-					question: 'Concentrated orange juice',
+					question: 'What do people traditionally put on top of a Christmas tree?',
+					answer: [
+						{value: 0, label: 'Snow Flak'},
+						{value: 1, label: 'Angle'},
+						{value: 0, label: 'Star'},
+						{value: 0, label: 'Cross'}
+					]
 				},
 				{
-					question: 'Banana peels makes great slippers.',
+					question: 'Which country did eggnog come from?',
+					answer: [
+						{value: 0, label: 'American'},
+						{value: 1, label: 'England'},
+						{value: 0, label: 'Britain'},
+						{value: 0, label: 'France'}
+					]
 				},
 			],
 		};
@@ -43,19 +82,23 @@
 			OutputCard(obj, index) {
 				if (!obj || !obj.hasOwnProperty('question')) return;
 				const uID = page + index;
-				let optionHtml = answer.map((a) =>
-					$(`<label><input type="radio" value="${a.value}" name="${uID}"> ${a.label} </label>`).on('change', function () {
-						$(this).parent().find('button').prop('disabled', false);
-					})
-				);
+				let answers = obj.hasOwnProperty('answer') ? obj.answer : genericAnswer;
+				if ()
+
+				let optionHtml = answers.map((a) => {
+					let label = $(`<p><label for="${uID+i}"> ${a.label} </label></p>`)
+					let input = $(`<input type="radio" value="${a.value}" name="${uID}" id="${uID+i}">`).on("change", function () {
+						$(this).parents('fieldset').find("button").prop("disabled", false);
+					});
+					return label.prepend(input);
+				});
 
 				let btn = $('<button>Next</button>')
 					.prop('disabled', true)
 					.click(function () {
 						return Quiz.NextQuestion(index);
 					});
-				// let className = index != 0 ? "hidden" : "";
-				// class="${className}"
+
 				let html = $(`<fieldset id="${uID}">` + `<h3>${index + 1}. ${obj.question}</h3>` + `</fieldset>`)
 					.append(optionHtml)
 					.append(btn)
@@ -64,7 +107,7 @@
 			},
 
 			CreateQuiz() {
-				$.each(quiz[page], function (index, value) {
+				$.each(quiz[testSet], function (index, value) {
 					qzHtml.push(Quiz.OutputCard(value, index));
 				});
 				$This.append(qzHtml[currentIndex].slideDown());
@@ -72,13 +115,24 @@
 
 			NextQuestion(index) {
 				currentIndex = index + 1;
-				$(qzHtml[index]).slideUp();
-				if (currentIndex == quiz[page].length) {
+				// console.log('currentIndex:', currentIndex, 'index:', index );
+				if (index !== -1){
+					$(qzHtml[index]).slideUp();
+				}
+				if (currentIndex >= qzHtml.length ){
 					Quiz.Tally();
 				} else {
-					$This.append(qzHtml[currentIndex]);
+					$This.append( qzHtml[currentIndex] );
 					qzHtml[currentIndex].slideDown();
-				}
+				};
+			},
+
+			FirstFrame(){
+				let btn = $("<button>Next</button>").click(function () {
+						return Quiz.NextQuestion(0);
+					});
+				let html = $(`<div><h1>Ready?</h1></div>`).append(btn);
+				return html;
 			},
 
 			Tally() {
@@ -89,7 +143,7 @@
 				const sum = answers.reduce((partial, a) => partial + a, 0);
 				console.log('Answers:', answers, 'Sum:', sum);
 
-				$This.append(`<h2>Total: ${sum}</h2>`);
+				$This.append(`<h2>Right Answers: ${sum}</h2>`);
 			},
 		};
 
